f55409c1b51dbc9d1ba7034e45efe2b1cd4d6e32
iuuukhueeee <nguyenducthien9@gmail.com>
2023-10-01 00:24:46 +0700

    Reworked Options component
    Now this component will display have no. and appear in more ui looking
    
    Signed-off-by: iuuukhueeee <nguyenducthien9@gmail.com>

--- src/components/multitple-choice.tsx
+++ src/components/multitple-choice.tsx
@@ -1,27 +1,39 @@
-import { Box, VStack } from 'native-base'
+import { VStack, Circle, Flex } from 'native-base'
 
 type Props = {
-  question: string
+  answer: string
+  no?: string
 }
 
-const QuestionBox = (props: Props) => {
-  const { question } = props
+const Options = (props: Props) => {
+  const { answer, no } = props
   return (
-    <Box rounded="xl" background="white" p={4} _text={{ color: '#04297A', fontSize: '14', lineHeight: '22' }}>
-      {question}
-    </Box>
+    <Flex
+      rounded="xl"
+      background="white"
+      py={3}
+      _text={{ color: '#04297A', fontSize: '14', lineHeight: '22' }}
+      flexDirection="row"
+      align="center"
+    >
+      <Circle size="40px" bg="#DFE3E8" mx={4} _text={{ color: '#212B36' }}>
+        {no}
+      </Circle>
+      {answer}
+    </Flex>
   )
 }
 
 const MultipleChoice = () => {
   return (
     <VStack space={'4'} flex={1} w="90%">
-      <QuestionBox question="hehe" />
-      <QuestionBox question="hehe" />
-      <QuestionBox question="hehe" />
-      <QuestionBox question="hehe" />
+      <Options answer="Answer 1" no="1" />
+      <Options answer="Answer 2" no="2" />
+      <Options answer="Answer 3" no="3" />
+      <Options answer="Answer 4" no="4" />
     </VStack>
   )
 }
 
 export default MultipleChoice
+export { Options }
