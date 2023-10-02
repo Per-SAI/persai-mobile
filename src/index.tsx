bcce641f3af700f2c36c08f67a9d5404eb367766
iuuukhueeee <nguyenducthien9@gmail.com>
2023-10-02 21:22:22 +0700

    Added: MyCollection screen
    Signed-off-by: iuuukhueeee <nguyenducthien9@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import MainScreen from './screens/main-screen'
 import FlashcardScreen from './screens/flashcard-screen'
 import LearnScreen from './screens/learn-screen'
 import MultipleTestScreen from './screens/multiple-test-screen'
+import MyCollectionScreen from './screens/my-collection-screen'
 
 const options = {
   headerShown: false
@@ -20,6 +21,7 @@ const App = () => {
       <Drawer.Screen name="Flashcard" component={FlashcardScreen} />
       <Drawer.Screen name="Learn" component={LearnScreen} />
       <Drawer.Screen name="MultipleTest" component={MultipleTestScreen} />
+      <Drawer.Screen name="MyCollection" component={MyCollectionScreen} />
     </Drawer.Navigator>
   )
 }
---
+++ src/screens/my-collection-screen.tsx
@@ -0,0 +1,54 @@
+import { Box, Center, Container, Flex, Text, ScrollView } from 'native-base'
+
+const MyCollectionScreen = () => (
+  <ScrollView>
+    <Center safeArea>
+      <Container w="100%" mt={3}>
+        <Flex
+          justifyContent="center"
+          borderColor="white"
+          borderRadius="md"
+          borderWidth={2}
+          w="100%"
+          bg='white'
+          h='16'
+          align='center'
+        >
+          <Text fontWeight='medium'>EXE201</Text>
+        </Flex>
+      </Container>
+
+      <Container w="100%" mt={3}>
+        <Flex
+          justifyContent="center"
+          borderColor="white"
+          borderRadius="md"
+          borderWidth={2}
+          w="100%"
+          bg='white'
+          h='16'
+          align='center'
+        >
+          <Text fontWeight='medium'>EXE201</Text>
+        </Flex>
+      </Container>
+
+      <Container w="100%" mt={3}>
+        <Flex
+          justifyContent="center"
+          borderColor="white"
+          borderRadius="md"
+          borderWidth={2}
+          w="100%"
+          bg='white'
+          h='16'
+          align='center'
+        >
+          <Text fontWeight='medium'>EXE201</Text>
+        </Flex>
+      </Container>
+    </Center>
+  </ScrollView>
+)
+
+export default MyCollectionScreen
