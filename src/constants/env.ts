1e77bb90242c82b075d911a57c06eb0920ac9589
iuuukhueeee <nguyenducthien9@gmail.com>
2023-10-10 23:54:52 +0700

    Chore: added env
    Signed-off-by: iuuukhueeee <nguyenducthien9@gmail.com>

---
+++ src/constants/env.ts
@@ -0,0 +1,4 @@
+export const WEB_CLIENT_ID =
+  '662805201143-rp4smdnac3id3g1b8cbofsb1ficimtr5.apps.googleusercontent.com'
+export const IOS_CLIENT_ID =
+  '662805201143-0kld4dfkcrtndnjo3sk2pjoepri0jjlu.apps.googleusercontent.com'
