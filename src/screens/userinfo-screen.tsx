c068910b85799de650b549f6d90c59a0492b143a
iuuukhueeee <nguyenducthien9@gmail.com>
2023-10-10 23:05:33 +0700

    Chore: added user information screen
    Signed-off-by: iuuukhueeee <nguyenducthien9@gmail.com>

---
+++ src/screens/userinfo-screen.tsx
@@ -0,0 +1,12 @@
+import { Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const UserInfoScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>UserInfo</Text>
+    </SafeAreaView>
+  )
+}
+
+export default UserInfoScreen
